d393c6eeddd7c1da3f758e78a710ca034e45c57c
Nickolas Moede <500139963@fvtc.edu>
2024-11-15 14:17:13 -0600

    completed the circle.h and circle.cpp

--- Shape/Circle.cpp
+++ Shape/Circle.cpp
@@ -1,7 +1,25 @@
 
 // Circle.cpp - You CAN edit this file!
+#include <math.h>
 
 #include "Circle.h"
 
 namespace { const float PI = 3.14159265359f; } // Use this to aproximate Pi
+
+void Circle::SetRadius(const float radius)
+{
+	if (radius > 0) m_radius = radius;
+	else m_radius = 1;
+}
+
+float Circle::GetArea() const
+{
+
+    return PI * (m_radius * m_radius);
+
+}
+float Circle::GetPerimeter() const
+{
+	return 2 * PI * m_radius;
+}
 
--- Shape/Circle.h
+++ Shape/Circle.h
@@ -9,20 +9,30 @@ class Circle : public Shape
 {
 private:
 
-	float m_radius = 1;
+	float m_radius = 1; 
 
 public:
 
 	Circle() { }
-	Circle(float radius) { SetRadius(radius); }
-	virtual ~Circle() { }
+	Circle(const float radius) { SetRadius(radius); } /*constructor sating that whenever Circle() is called with a float argument passed 
+														It will take the radius and set it to m_radius else it will just stay = 1.*/
+	virtual ~Circle() { } //destructor
 
 	// Accessor
-	virtual float GetRadius() { return m_radius; }
+	virtual float GetRadius() { return m_radius; } //getter
 
 	// Mutator
 
+	virtual void SetRadius(const float radius); //setter
+
+
+    virtual std::string GetType() const override { return "Circle"; } /*Can and will be overwritten from the base class Shape "Shape". 
+																		Easy to do in header file because of Length of code using the ovveride keyword.*/
+
 	// Define additional methods here:
 
+	virtual float GetArea() const; //must be overwritten and that is done in Circle.cpp
+
+	virtual float GetPerimeter() const; //must be overwritten and that is done in Circle.cpp
 
 };
